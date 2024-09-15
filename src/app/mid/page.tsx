88e7941bb9f40f519aedae3a34f9f0968225a35f
直樹 稲岡 <C4030@oic.jp>
2024-09-15 13:45:30 +0900

    ログイン選択画面の追加

--- src/app/mid/page.tsx
+++ src/app/mid/page.tsx
@@ -1,26 +1,34 @@
+"use client";
+
 import Link from "next/link";
 
-export default function TopPage() {
+export default function MidPage() {
   return (
     <div className="min-h-screen bg-white flex flex-col justify-between items-center p-6">
       <div className="flex-grow flex items-center justify-center">
         <h1 className="text-5xl font-bold text-gray-800">Login Game</h1>
       </div>
       <div className="flex-grow flex items-center justify-center">
-        <h1 className="text-3xl font-bold text-gray-800">Play Now</h1>
+        <h1 className="text-3xl font-bold text-gray-800">Select Login</h1>
       </div>
       <div className="mb-6 space-y-4">
         <Link
-          href="/mid"
+          href="/login"
           className="block w-full max-w-md mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
         >
           Login
         </Link>
         <Link
-          href="/register"
-          className="block w-full max-w-md mx-auto bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded text-center"
+          href="/test"
+          className="block w-full max-w-md mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
         >
-          Create new account
+          Login Test1
+        </Link>
+        <Link
+          href="/voice"
+          className="block w-full max-w-md mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
+        >
+          Voice Login
         </Link>
       </div>
     </div>
